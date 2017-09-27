2250e4d6c1f98af80ac7d227eaba7fcae3ee1c3b
huoxiaoyi <huoxiaoyi@didapinche.com>
2017-09-27 10:08:33 +0800

    Google官方文档实践 - Resources - finish.

--- app/src/main/java/com/huoxy/googleofficialpractice/apiguide/chapter2/AppResourcesActivity.java
+++ app/src/main/java/com/huoxy/googleofficialpractice/apiguide/chapter2/AppResourcesActivity.java
@@ -2,10 +2,13 @@ package com.huoxy.googleofficialpractice.apiguide.chapter2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
 
 import com.huoxy.googleofficialpractice.R;
 
 public class AppResourcesActivity extends AppCompatActivity {
+
+    AutoCompleteTextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
